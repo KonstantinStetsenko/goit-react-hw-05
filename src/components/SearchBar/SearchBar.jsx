653fb36b5bc04f067bc8efd61a0c7a3cae661b13
KonstantinStetsenko <160874184+KonstantinStetsenko@users.noreply.github.com>
2024-11-23 02:36:44 +0200

    end 23-11-24

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,10 @@
 import { Formik, Form, Field } from "formik";
 import { fetchFilmGallerySearch } from "./../../../services/filmGallery-API";
+import css from "./SearchBar.module.css"
+import Container from "../../../Container/Container";
+
+
+
 
 export default function SearchBar({ handleSetQuery}) {
   const handleSubmit = (values, options) => {
@@ -12,13 +17,13 @@ export default function SearchBar({ handleSetQuery}) {
     query: "",
   };
   return (
-    <>
-      <Formik onSubmit={handleSubmit} initialValues={initialValues}>
-        <Form>
-          <Field name="query" placeholder="Search film ..." />
+    <Container>
+      <Formik className={css.formContainer } onSubmit={handleSubmit} initialValues={initialValues}>
+        <Form className={css.formSearch}>
+          <Field className={ css.inputSearch} name="query" placeholder="Search film ..." />
           <button type="submit">Search</button>
         </Form>
       </Formik>
-    </>
+    </Container>
   );
 }
